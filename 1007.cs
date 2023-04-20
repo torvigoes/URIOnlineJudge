d26a011e5bd3242197899ac85b0a7723a7da21b7
Vitor Neves Góes <77070562+torvigoes@users.noreply.github.com>
2023-04-20 18:51:39 -0300

    Update 1007.cs

--- 1007.cs
+++ 1007.cs
@@ -4,17 +4,17 @@ using System;
 namespace URIOnlineJudge{
 	class _1007{
 		static void Main(string[] args){
-            int nf; int ht;
-            double wageHour;
+		    int nf; int ht;
+		    double wageHour;
 
-            nf = Convert.ToInt32(Console.ReadLine());
-            ht = Convert.ToInt32(Console.ReadLine());
-            wageHour = Convert.ToDouble(Console.ReadLine());
+		    nf = Convert.ToInt32(Console.ReadLine());
+		    ht = Convert.ToInt32(Console.ReadLine());
+		    wageHour = Convert.ToDouble(Console.ReadLine());
 
-            double wage = wageHour * ht;
+		    double wage = wageHour * ht;
 
-            Console.WriteLine($"NUMBER = {nf}");
-            Console.WriteLine($"SALARY = U$ {wage.ToString("0.00")}");
+		    Console.WriteLine($"NUMBER = {nf}");
+		    Console.WriteLine($"SALARY = U$ {wage.ToString("0.00")}");
 
 
 		}
